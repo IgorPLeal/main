27fc3877c2ff762270fd49584a274eaceb481f72
igor.pleal <igorpleal@gmail.com>
2018-09-20 21:37:53 -0300

    Exericio 01

--- ex/ex.py
+++ ex/ex.py
@@ -1,25 +1,21 @@
-import math
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+n3 = int(input("Terceiro número: "))
 
-n1 = input("Digite o 1° numero: ")
-n2 = input("Digite o 2° numero: ")
-n3 = input("Digite o 3° numero: ")
+maior = n1
+menor = n1
 
-def maior():
-    if n1 > n2 > n3:
-        print("O maior numero e: ", n1, "O menor numero e", n3)
-    elif n2 > n1 > n3:
-        print (n2,"é o maior numero!!")
-    elif n3 > n1 > n2:
-        print n3 ("é o maior numero!!")
+if maior < n2:
+	maior = n2
 
-    #Se alguns numeros forem iguais
+if maior < n3:
+	maior = n3
 
-    elif n1 == n2 and n1 and n2 > n3:
-        print (n1,"e o maior")
-    elif n1 == n3 and n1 and n3 > n2:
-        print(n1,"é o maior!!")
-    elif n2 == n3 and n2 and n3 > n1:
-        print (n2,"é o maior!!")
-    #todos os numeros iguais
-    elif n1 == n2 and n3:
-        print ("todos o numeros são iguais")
+if menor > n2:
+	menor = n2
+
+if menor > n3:
+	menor = n3
+
+print ('Maior:  %d ' %maior)
+print ('Menor:  %d ' %menor)
